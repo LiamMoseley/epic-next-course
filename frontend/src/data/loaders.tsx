6ff41e57f6cdeaf98ab20bc209e9cd7c72ac67a6
LiamMoseley <liam.martin.moseley@gmail.com>
2024-06-22 15:16:57 +0100

    Custom error page.

--- frontend/src/data/loaders.tsx
+++ frontend/src/data/loaders.tsx
@@ -51,6 +51,8 @@ export async function getGlobalData() {
 }
 
 export async function getHomePageData() {
+  throw new Error("Test error");
+
   const url = new URL("/api/home-page", baseUrl);
 
   url.search = qs.stringify({
